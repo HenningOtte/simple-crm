0098b055894b8bf3017940afaab311053fca805f
Henning Otte <HenningOtte91@gmail.com>
2026-02-10 16:46:29 +0100

    Add User model and integrate it into the Add User form

--- src/app/dialog-add-user/dialog-add-user.ts
+++ src/app/dialog-add-user/dialog-add-user.ts
@@ -6,6 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule, MatCalendarCellClassFunction } from '@angular/material/datepicker';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { User } from '../../models/user.class';
 
 @Component({
   selector: 'app-dialog-add-user',
@@ -24,15 +25,23 @@ import { provideNativeDateAdapter } from '@angular/material/core';
   providers: [provideNativeDateAdapter()],
 })
 export class DialogAddUser {
+  user = new User();
+  birthdate?: Date;
+
+  saveUser() {
+    if (this.birthdate) {
+      this.user.birthDate = this.birthdate?.getTime();
+    }
+    console.log('Current User: ', this.user);
+  }
+
   dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
     // Only highligh dates inside the month view.
     if (view === 'month') {
       const date = cellDate.getDate();
-
       // Highlight the 1st and 20th day of each month.
       return date === 1 || date === 20 ? 'example-custom-date-class' : '';
     }
-
     return '';
   };
 }
