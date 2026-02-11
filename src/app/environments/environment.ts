2e27e0e059a1b803dd201ed94417e15be3e41f96
Henning Otte <HenningOtte91@gmail.com>
2026-02-11 16:01:48 +0100

    Fix Firebase setup & environment config

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const environment = {
+  apiKey: 'AIzaSyDxmomT2NfqW7Gy3n5AcnoatWIaySq1ku4',
+  authDomain: 'simple-crm-eb54a.firebaseapp.com',
+  projectId: 'simple-crm-eb54a',
+  storageBucket: 'simple-crm-eb54a.firebasestorage.app',
+  messagingSenderId: '765543409806',
+  appId: '1:765543409806:web:d1e238af07113b0ed601df',
+};
